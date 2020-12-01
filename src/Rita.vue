7a50ec2b11af8237d45b1b2412d6ea50aaf91bf1
Son <son@chillbits.com>
2020-12-01 09:59:25 +0000

    First

---
+++ src/Rita.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Rita</h1>
+  <HelloWorld msg="Rita init successfully." />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'Rita',
+  components: {
+    HelloWorld
+  }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import Rita from './Rita.vue'
+import './index.css'
+
+createApp(Rita).mount('#rita')
